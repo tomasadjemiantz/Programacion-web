bac3a0cc090ef16086ec3ec0596f93b4ee203418
Tomas} <tadjemiantz@itba.edu.ar>
2022-06-16 16:10:52 -0300

    pw

---
+++ Mi pagina web/codigo.js
@@ -0,0 +1,6 @@
+<!-- Genero: ProgramasProgramacion.com
+function MiNombreFuncion()
+{
+document.writeln("<a href=\"javascript:void(0)\" onclick=\"w3_close()\" class=\"w3-bar-item w3-button w3-display-topright w3-text-orange\">Close");
+}
+//-->
