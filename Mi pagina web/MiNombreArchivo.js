fc194e81e53496f2fec33591c586a669617b3f2a
Tomas} <tadjemiantz@itba.edu.ar>
2022-06-20 20:58:41 -0300

    pw

--- Mi pagina web/MiNombreArchivo.js
+++ Mi pagina web/MiNombreArchivo.js
@@ -1,3 +1,4 @@
+Genero: ProgramasProgramacion.com
 function MiNombreFuncion()
 {
 document.writeln(" <a href=\"javascript:void(0)\" onclick=\"w3_close()\" class=\"w3-bar-item w3-button w3-display-topright w3-text-orange\">Close");
