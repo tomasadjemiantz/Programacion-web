14958b334918a78e3205c6b69b6391c1f0e51854
Tomas} <tadjemiantz@itba.edu.ar>
2022-06-21 19:04:00 -0300

    pw

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-let horas = 22;
-let minutos = 0;
-let segundos = 0;
-cargarSegundo();
+
+  let horas = 22;
+  let minutos = 0;
+  let segundos = 0;
+  cargarSegundo();
 
 //segundos
 function cargarSegundo() {
-  let txtSegundos;
+
+  let txtSegundos =0;
 
   if (segundos < 0) {
     segundos = 59;
   }
 
   //segundos en pantalla
-  if (segundos < 10) {
-    txtSegundos = `0${segundos}`;
+   if (segundos < 10) {
+    txtSegundos = "0"+segundos;
+    document.getElementById("segundos").innerHTML = txtSegundos;
   } else {
     txtSegundos = segundos;
-  }
-  document.getElementById("segundos").innerHTML = txtSegundos;
-  segundos--;
 
+    document.getElementById("segundos").innerHTML = txtSegundos;
+  } 
+  segundos--;
   cargarMinutos(segundos);
 }
 
 //minutos
 function cargarMinutos(segundos) {
   let txtMinutos;
-
   if (segundos == -1 && minutos !== 0) {
     setTimeout(() => {
       minutos--;
@@ -50,7 +52,6 @@ function cargarMinutos(segundos) {
 //horas
 function cargarHoras(segundos, minutos) {
   let txtHoras;
-
   if (segundos == -1 && minutos == 0 && horas !== 0) {
     setTimeout(() => {
       horas--;
@@ -67,8 +68,8 @@ function cargarHoras(segundos, minutos) {
   } else {
     txtHoras = horas;
   }
-  document.getElementById("horas").innerHTML = txtHoras;
+  document.getElementById("horas").innerHML = txtHoras;
 }
 
 //cada segundo
-setInterval(cargarSegundo, 1000);
+setInterval(cargarSegundo,1);
