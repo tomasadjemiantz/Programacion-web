8f5db33001e231d5e5a41e75e6080bf742950cbb
Tomas} <tadjemiantz@itba.edu.ar>
2022-06-21 19:28:47 -0300

    pw

--- main.js
+++ main.js
@@ -1,46 +1,22 @@
-
- //===
-// VARIABLES
-//===
-const DATE_TARGET = new Date('30/06/2022 0:01 AM');
-// DOM for render
-const SPAN_DAYS = document.querySelector('span#days');
-const SPAN_HOURS = document.querySelector('span#hours');
-const SPAN_MINUTES = document.querySelector('span#minutes');
-const SPAN_SECONDS = document.querySelector('span#seconds');
-// Milliseconds for the calculations
-const MILLISECONDS_OF_A_SECOND = 1000;
-const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
-const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
-const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
-
-//===
-// FUNCTIONS
-//===
-
-/**
- * Method that updates the countdown and the sample
- */
-function updateCountdown() {
-    // Calcs
-    const NOW = new Date()
-    const DURATION = DATE_TARGET - NOW;
-    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
-    const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
-    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
-    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
-    // Thanks Pablo Monteserín (https://pablomonteserin.com/cuenta-regresiva/)
-
-    // Render
-    SPAN_DAYS.textContent = REMAINING_DAYS;
-    SPAN_HOURS.textContent = REMAINING_HOURS;
-    SPAN_MINUTES.textContent = REMAINING_MINUTES;
-    SPAN_SECONDS.textContent = REMAINING_SECONDS;
-}
-
-//===
-// INIT
-//===
-updateCountdown();
-// Refresh every second
-setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
+var deadline = new Date("dec 31, 2017 15:37:25").getTime();
+  
+var x = setInterval(function() {
+  
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("day").innerHTML =days ;
+document.getElementById("hour").innerHTML =hours;
+document.getElementById("minute").innerHTML = minutes; 
+document.getElementById("second").innerHTML =seconds; 
+if (t < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "TIME UP";
+        document.getElementById("day").innerHTML ='0';
+        document.getElementById("hour").innerHTML ='0';
+        document.getElementById("minute").innerHTML ='0' ; 
+        document.getElementById("second").innerHTML = '0'; }
+}, 1000);
