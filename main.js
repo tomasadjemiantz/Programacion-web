7750f2b370ddeff34a15c272e9ea3af77548f319
Tomas} <tadjemiantz@itba.edu.ar>
2022-06-21 12:36:56 -0300

    pw

--- main.js
+++ main.js
@@ -1,26 +1,74 @@
-const days = document.getElementById("days");
-const hours = document.getElementById("hours");
-const mins = document.getElementById("mins");
-const seconds = document.getElementById("seconds");
+let horas = 22;
+let minutos = 0;
+let segundos = 0;
+cargarSegundo();
 
-const newYear = "11 Jul 2022";
+//segundos
+function cargarSegundo() {
+  let txtSegundos;
 
-function countTimer() {
-  const newYearDate = new Date(newYear);
-  const currentDate = new Date();
+  if (segundos < 0) {
+    segundos = 59;
+  }
 
-  const totalSeconds = (newYearDate - currentDate) / 1000;
+  //segundos en pantalla
+  if (segundos < 10) {
+    txtSegundos = `0${segundos}`;
+  } else {
+    txtSegundos = segundos;
+  }
+  document.getElementById("segundos").innerHTML = txtSegundos;
+  segundos--;
 
-  const daysCalc = Math.floor(totalSeconds / 3600 / 24);
-  const hoursCalc = Math.floor(totalSeconds / 3600) % 24;
-  const minsCalc = Math.floor(totalSeconds / 60) % 60;
-  const secondsCalc = Math.floor(totalSeconds % 60);
-
-  days.innerHTML = daysCalc;
-  hours.innerHTML = hoursCalc;
-  mins.innerHTML = minsCalc;
-  seconds.innerHTML = secondsCalc;
+  cargarMinutos(segundos);
 }
-countTimer();
 
-setInterval(countTimer, 1000);
+//minutos
+function cargarMinutos(segundos) {
+  let txtMinutos;
+
+  if (segundos == -1 && minutos !== 0) {
+    setTimeout(() => {
+      minutos--;
+    }, 500);
+  } else if (segundos == -1 && minutos == 0) {
+    setTimeout(() => {
+      minutos = 59;
+    }, 500);
+  }
+
+  //mostrar minutos en pantalla
+  if (minutos < 10) {
+    txtMinutos = `0${minutos}`;
+  } else {
+    txtMinutos = minutos;
+  }
+  document.getElementById("minutos").innerHTML = txtMinutos;
+  cargarHoras(segundos, minutos);
+}
+
+//horas
+function cargarHoras(segundos, minutos) {
+  let txtHoras;
+
+  if (segundos == -1 && minutos == 0 && horas !== 0) {
+    setTimeout(() => {
+      horas--;
+    }, 500);
+  } else if (segundos == -1 && minutos == 0 && horas == 0) {
+    setTimeout(() => {
+      horas = 2;
+    }, 500);
+  }
+
+  //Horas en pantalla
+  if (horas < 10) {
+    txtHoras = `0${horas}`;
+  } else {
+    txtHoras = horas;
+  }
+  document.getElementById("horas").innerHTML = txtHoras;
+}
+
+//cada segundo
+setInterval(cargarSegundo, 1000);
