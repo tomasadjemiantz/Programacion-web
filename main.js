227a2d9779f2d0eb01fdccfd4efd62961c3d7b6e
Tomas} <tadjemiantz@itba.edu.ar>
2022-06-21 19:25:47 -0300

    pw

--- main.js
+++ main.js
@@ -1,75 +1,46 @@
 
-  let horas = 22;
-  let minutos = 0;
-  let segundos = 0;
-  cargarSegundo();
+ //===
+// VARIABLES
+//===
+const DATE_TARGET = new Date('04/13/2020 0:01 AM');
+// DOM for render
+const SPAN_DAYS = document.querySelector('span#days');
+const SPAN_HOURS = document.querySelector('span#hours');
+const SPAN_MINUTES = document.querySelector('span#minutes');
+const SPAN_SECONDS = document.querySelector('span#seconds');
+// Milliseconds for the calculations
+const MILLISECONDS_OF_A_SECOND = 1000;
+const MILLISECONDS_OF_A_MINUTE = MILLISECONDS_OF_A_SECOND * 60;
+const MILLISECONDS_OF_A_HOUR = MILLISECONDS_OF_A_MINUTE * 60;
+const MILLISECONDS_OF_A_DAY = MILLISECONDS_OF_A_HOUR * 24
 
-//segundos
-function cargarSegundo() {
+//===
+// FUNCTIONS
+//===
 
-  let txtSegundos = 0;
+/**
+ * Method that updates the countdown and the sample
+ */
+function updateCountdown() {
+    // Calcs
+    const NOW = new Date()
+    const DURATION = DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor((DURATION % MILLISECONDS_OF_A_DAY) / MILLISECONDS_OF_A_HOUR);
+    const REMAINING_MINUTES = Math.floor((DURATION % MILLISECONDS_OF_A_HOUR) / MILLISECONDS_OF_A_MINUTE);
+    const REMAINING_SECONDS = Math.floor((DURATION % MILLISECONDS_OF_A_MINUTE) / MILLISECONDS_OF_A_SECOND);
+    // Thanks Pablo Monteserín (https://pablomonteserin.com/cuenta-regresiva/)
 
-  if (segundos < 0) {
-    segundos = 59;
-  }
-
-  //segundos en pantalla
-   if (segundos < 10) {
-    txtSegundos = "0"+segundos;
-    document.getElementById("segundos").innerHTML = txtSegundos;
-  } else {
-    txtSegundos = segundos;
-
-    document.getElementById("segundos").innerHTML = txtSegundos;
-  } 
-  segundos--;
-  cargarMinutos(segundos);
+    // Render
+    SPAN_DAYS.textContent = REMAINING_DAYS;
+    SPAN_HOURS.textContent = REMAINING_HOURS;
+    SPAN_MINUTES.textContent = REMAINING_MINUTES;
+    SPAN_SECONDS.textContent = REMAINING_SECONDS;
 }
 
-//minutos
-function cargarMinutos(segundos) {
-  let txtMinutos;
-  if (segundos == -1 && minutos !== 0) {
-    setTimeout(() => {
-      minutos--;
-    }, 500);
-  } else if (segundos == -1 && minutos == 0) {
-    setTimeout(() => {
-      minutos = 59;
-    }, 500);
-  }
-
-  //mostrar minutos en pantalla
-  if (minutos < 10) {
-    txtMinutos = `0${minutos}`;
-  } else {
-    txtMinutos = minutos;
-  }
-  document.getElementById("minutos").innerHTML = txtMinutos;
-  cargarHoras(segundos, minutos);
-}
-
-//horas
-function cargarHoras(segundos, minutos) {
-  let txtHoras;
-  if (segundos == -1 && minutos == 0 && horas !== 0) {
-    setTimeout(() => {
-      horas--;
-    }, 500);
-  } else if (segundos == -1 && minutos == 0 && horas == 0) {
-    setTimeout(() => {
-      horas = 2;
-    }, 500);
-  }
-
-  //Horas en pantalla
-  if (horas < 10) {
-    txtHoras = `0${horas}`;
-  } else {
-    txtHoras = horas;
-  }
-  document.getElementById("horas").innerHML = txtHoras;
-}
-
-//cada segundo
-setInterval(cargarSegundo,1);
+//===
+// INIT
+//===
+updateCountdown();
+// Refresh every second
+setInterval(updateCountdown, MILLISECONDS_OF_A_SECOND);
